7796949e75188d863c021cd1a793c2f7cb1215de
phuongnam12a4gt <phuongnam12a4gt@gmail.com>
2021-03-28 13:01:10 +0700

    tHIEt ke man hinh splash vs kiem tra du lieu

--- app/src/main/java/com/example/sos__vn/screen/HomeActivity.kt
+++ app/src/main/java/com/example/sos__vn/screen/HomeActivity.kt
@@ -1,11 +1,12 @@
-package com.example.sos__vn
+package com.example.sos__vn.screen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sos__vn.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
---
+++ app/src/main/java/com/example/sos__vn/screen/SplashActivity.kt
@@ -0,0 +1,33 @@
+package com.example.sos__vn.screen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
+import com.example.sos__vn.R
+import com.example.sos__vn.utils.AppUtils
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        loadData()
+    }
+
+    private fun loadData() {
+        if (AppUtils.isNetWorkAvailable(this)) {
+            Handler().postDelayed(object : Runnable {
+                override fun run() {
+                    var intent = Intent(this@SplashActivity, HomeActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+
+            }, 3000)
+            Toast.makeText(this, "NetWork Connect ", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "NetWork DisConnect ", Toast.LENGTH_LONG).show()
+        }
+    }
+}
